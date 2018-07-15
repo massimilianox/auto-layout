16ec199da507dfb62bc5d0975431b19df24caf43
Mamo <m.abeli@t4project.com>
2018-07-15 10:18:58 +0200

    Auto Layout for iPhone

---
+++ auto-layout/auto-layout/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  auto-layout
+//
+//  Created by Massimiliano Abeli on 15/07/2018.
+//  Copyright © 2018 Massimiliano Abeli. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
